5bf4dcf897b8988b05c301727aef56bb3dc65528
kevin <kevinG>
2026-06-07 13:37:08 -0500

    se importo router y css

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// ---> importsaciones para todo los estilos
+import './style.css'
+// ----> importacion del router
+import renderisado from './router/router'
+
+// ---> capturar el router
+document.addEventListener("DOMContentLoaded", renderisado)
